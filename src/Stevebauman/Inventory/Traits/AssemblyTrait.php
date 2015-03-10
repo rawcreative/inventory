3aafadd711dff610ad5710daf9dee5b1d6e17735
T. Debo <tim@rawcreativestudios.com>
2015-03-10 10:35:30 -0400

    fix formatting

--- src/Stevebauman/Inventory/Traits/AssemblyTrait.php
+++ src/Stevebauman/Inventory/Traits/AssemblyTrait.php
@@ -2,49 +2,63 @@
 
 namespace Stevebauman\Inventory\Traits;
 
-trait AssemblyTrait 
+trait AssemblyTrait
 {
-	
-	protected $inventoryAssembly = 'Stevebauman\Inventory\Models\InventoryAssembly';
-	
-	public function makeAssembly() {
-		
-		$assembly = new $this->inventoryAssembly([
-								'inventory_id' => $this->id, 
-								'part_id' => $this->id, 
-								'depth' => 0
-								]);
-		$assembly->save();
-			
-		$this->assembly = true;
 
-		return $this;
-	}
+    protected $inventoryAssembly = 'Stevebauman\Inventory\Models\InventoryAssembly';
 
-	public function getAssemblyItems()
+    /**
+     * @return mixed
+     */
+    public function makeAssembly()
+    {
+
+        $assembly = new $this->inventoryAssembly([
+            'inventory_id' => $this->id,
+            'part_id' => $this->id,
+            'depth' => 0,
+        ]);
+        $assembly->save();
+
+        $this->assembly = true;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAssemblyItems()
     {
         $id = $this->id;
-        return $this->select('inventories.*', 'inventory_assemblies.quantity')->join('inventory_assemblies', function($join) use ($id) {
+        return $this->select('inventories.*', 'inventory_assemblies.quantity')->join('inventory_assemblies', function ($join) use ($id) {
             $join->on('inventories.id', '=', 'inventory_assemblies.part_id')
-                ->where('inventory_assemblies.inventory_id', '=', $id)
-                ->where('inventory_assemblies.depth', '>', 0);
+            ->where('inventory_assemblies.inventory_id', '=', $id)
+            ->where('inventory_assemblies.depth', '>', 0);
         })->get();
     }
 
-    public function addAssemblyItem($part, $qty = 1, $depth = null, $returnPart = false) {
-    	
-    	if ($this->exists) {
-    		
+    /**
+     * @param $part
+     * @param $qty
+     * @param $depth
+     * @param null $returnPart
+     */
+    public function addAssemblyItem($part, $qty = 1, $depth = null, $returnPart = false)
+    {
+
+        if ($this->exists) {
+
             if (is_null($depth)) {
                 $depth = 1;
             }
 
             $partAssembly = new $this->inventoryAssembly([
-					            	'inventory_id' => $this->id, 
-					            	'part_id' => $part->id, 
-					            	'quantity' => $qty, 
-					            	'depth' => $depth
-					            	]);
+                'inventory_id' => $this->id,
+                'part_id' => $part->id,
+                'quantity' => $qty,
+                'depth' => $depth,
+            ]);
 
             $partAssembly->save();
         }
@@ -52,14 +66,19 @@ trait AssemblyTrait
         return ($returnPart === true ? $part : $this);
     }
 
+    /**
+     * @param $parts
+     */
+    public function addAssemblyItems(array $parts)
+    {}
 
-    public function addAssemblyItems(array $parts) {}
-
-   
-    public function scopeAssembly($query) {
+    /**
+     * @param $query
+     * @return mixed
+     */
+    public function scopeAssembly($query)
+    {
         return $query->where('assembly', '=', true);
     }
-
-
 
 }
